b69a66cd7b206271675a63dcf59c3711aa58e4fa
Ram Lalu <laluram54@gmail.com>
2019-11-18 18:20:45 +0530

    commit 1

---
+++ WcfDemo/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfDemo
+{
+    [DataContract]
+    public class Employee
+    {
+        [DataMember]
+        public int EmployeeID { get; set; }
+        [DataMember]
+        public string FirstName { get; set; }
+        [DataMember]
+        public string LastName { get; set; }
+        [DataMember]
+        public int Age { get; set; }
+    }
+}
---
+++ WcfDemo/Service1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfDemo
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {      
+        public List<Employee> GetEmployeeData()
+        {
+            List<Employee> lstEmployees = new List<Employee>() { 
+            
+                            new Employee {EmployeeID=1,FirstName="John",LastName="Cary",Age=23},
+                            new Employee {EmployeeID=2,FirstName="Black",LastName="Smith",Age=25},
+                            new Employee {EmployeeID=3,FirstName="Mike",LastName="Charles",Age=33}
+            };
+
+            return lstEmployees;
+        }
+    }
+}
